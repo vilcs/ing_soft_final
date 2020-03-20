141c5096fd96ae38bdc4649a60ada668cf332c00
Ernesto Campohermoso <eo.campohermoso@acad.ucb.edu.bo>
2020-03-19 20:25:29 -0400

    Primera versión del proyecto

---
+++ src/test/java/bo/edu/ucb/sis/piratebay/PiratebayApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis.piratebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratebayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
