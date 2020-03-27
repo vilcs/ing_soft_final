20232f021871d0c057d624879c84c71a14287796
Ernesto Campohermoso <eo.campohermoso@acad.ucb.edu.bo>
2020-03-26 21:03:49 -0400

    ABM de cliente

---
+++ src/main/java/bo/edu/ucb/sis/piratebay/AppConfiguration.java
@@ -0,0 +1,22 @@
+package bo.edu.ucb.sis.piratebay;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@MapperScan(value="bo.edu.ucb.sis.piratebay", sqlSessionFactoryRef = "sqlSessionFactory")
+public class AppConfiguration {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
+}
