c09072cd28ad1707da67cd43eece499276281f99
Ernesto Campohermoso <eo.campohermoso@acad.ucb.edu.bo>
2020-03-26 21:12:34 -0400

    Subiendo archivo HTTP para probar endpoint de guardado de clientes

--- src/main/java/bo/edu/ucb/sis/piratebay/ClientController.java
+++ src/main/java/bo/edu/ucb/sis/piratebay/ClientController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
